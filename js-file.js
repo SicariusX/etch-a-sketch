f4b4c01c412f015262aaebb9fc8f3d7500c443d3
TK Khor <khortzherkang@gmail.com>
2022-04-02 16:09:56 +0800

    functional game with option to reset value

--- js-file.js
+++ js-file.js
@@ -1,55 +1,39 @@
 let container=document.querySelector(".container");
 
 
-for (let i=0;i<256;i++){
-    let gridbox= document.createElement("div");
-    gridbox.style.cssText="border:1px solid black; height:60px; width:60px";
-    container.appendChild(gridbox);
- }
 
+function createGrid(){
+for (let i=0;i<x;i++){
+    let xAxis= document.createElement("div");
+    let divHeightandWidth=(800/x);
+    xAxis.style.cssText=`border:1px solid black;height:${divHeightandWidth}px;width:${divHeightandWidth}px`;
+    container.appendChild(xAxis);
+for (let i=0;i<x;i++) {
+    let yAxis=document.createElement("div");
+    yAxis.style.cssText=`border:1px solid black;height:${divHeightandWidth}px;width:${divHeightandWidth}px`;
+    xAxis.appendChild(yAxis);
+    }  
+   }
+}
 
 let div=document.querySelector("div");
-//div.addEventListener("click",function (e){  
-//console.log(e.target)
-// }); to test if it is working
-
 div.addEventListener("mouseover",function (e){
-    e.target.style.background="black";  //e to target the divs.
+    e.target.style.background="black"; 
 })
 
 
 let reload=document.querySelector(".reload");
-reload.addEventListener("click",function(){
- location.reload();
- askforValue();
-});
+reload.addEventListener("click", resetButton)
+
+function resetButton () {
+  x= parseInt(prompt("Please enter value size for gridbox sizing"));
+  eraseGrid();
+  createGrid();
+}
 
 function eraseGrid() {
 container.textContent="";
 }
 
-function askforValue() {
-  let x= parseInt(prompt("Please enter value choice for gridbox sizing-16 (default), 10, 20 or 50"));
-  if (x===10)  {
-    eraseGrid();
-    for (let i=0;i<100;i++) {
-        let gridbox1= document.createElement("div");
-        container.appendChild(gridbox1);
-        gridbox1.style.cssText="border:1px solid black; height:60px; width:60px";
-        container.style.cssText="grid-template-columns: repeat(10)"
-     }
-   }
-};
-
-
-
-let ten=document.querySelector(".tengrid");
-ten.addEventListener("click",function(){
-    eraseGrid();
-    for (let i=0;i<100;i++) {
-        let gridbox1= document.createElement("div");
-         container.appendChild(gridbox1);
-         gridbox1.style.cssText="border:1px solid black; height:60px; width:60px";
-         container.style.cssText="grid-template-columns: repeat(10)";
-    }
-});
+let x=16;
+createGrid();
