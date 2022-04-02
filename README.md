f56da16a48ffb1ad61114b1fc04ce2c19e3b38ed
TK Khor <khortzherkang@gmail.com>
2022-04-02 21:03:02 +0800

    added comments to js file and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+
+Classic game where the player can hover around and it leaves a trail. Player can also select the grid by determining the no. of square divs. 
--- js-file.js
+++ js-file.js
@@ -1,20 +1,18 @@
 let container=document.querySelector(".container");
 
-
-
 function createGrid(){
-for (let i=0;i<x;i++){
-    let xAxis= document.createElement("div");
-    let divHeightandWidth=(800/x);
-    xAxis.style.cssText=`border:1px solid black;height:${divHeightandWidth}px;width:${divHeightandWidth}px`;
-    container.appendChild(xAxis);
-for (let i=0;i<x;i++) {
-    let yAxis=document.createElement("div");
-    yAxis.style.cssText=`border:1px solid black;height:${divHeightandWidth}px;width:${divHeightandWidth}px`;
-    xAxis.appendChild(yAxis);
-    }  
-   }
-}
+  for (let i=0;i<x;i++){
+        let createDivs= document.createElement("div"); //creates the square divs
+        let divHeightandWidth=(800/x);
+        createDivs.style.cssText=`border:1px solid black;height:${divHeightandWidth}px;width:${divHeightandWidth}px`;
+        container.appendChild(createDivs);
+        for (let i=0;i<x;i++) {
+          let iterationOfLoops=document.createElement("div"); //how many loops do we want of the divs?
+          iterationOfLoops.style.cssText=`border:1px solid black;height:${divHeightandWidth}px;width:${divHeightandWidth}px`;
+          createDivs.appendChild(iterationOfLoops);
+        }  
+    }
+ }
 
 let div=document.querySelector("div");
 div.addEventListener("mouseover",function (e){
@@ -26,7 +24,7 @@ let reload=document.querySelector(".reload");
 reload.addEventListener("click", resetButton)
 
 function resetButton () {
-  x= parseInt(prompt("Please enter value size for gridbox sizing"));
+  x= parseInt(prompt("Please enter value size for gridbox sizing (10-100)"));
   eraseGrid();
   createGrid();
 }
